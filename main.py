bdf7f6378ed38b65f7a4a3d95ba9a2fb71c125c1
Rahul Golani <rahulgolani24@live.com>
2020-01-04 19:36:19 +0530

    Initial Commit

---
+++ main.py
@@ -0,0 +1,54 @@
+import requests
+import json
+import pprint
+from datetime import datetime
+class Score:
+
+    def __init__(self):
+        '''Initializing all the required endpoints '''
+        self.getAllMatches="http://cricapi.com/api/matches/"
+        self.getScore="http://cricapi.com/api/cricketScore"
+        self.apiKey="fLehGQotgcMvU5o7z4DYbXCi0qE2"
+        self.uniqueId=""
+
+    def getUniqueId(self):
+        uriParams={"apikey":self.apiKey}
+        resp=requests.get(self.getAllMatches,params=uriParams)
+        respDict=resp.json()
+        uIdFound=0
+        #pprint.pprint(respDict)
+        for i in respDict['matches']:
+            if (i['team-1']=='Australia' or i['team-2']=='Australia' and i['matchStarted']):
+                #todaysDate=datetime.today().strftime('%Y-%m-%d')
+                '''
+                if todaysDate==i['date'].split('T')[0]:
+                    self.uniqueId=i['unique_id']
+                '''
+                self.uniqueId=i['unique_id']
+                uIdFound=1
+                break
+        if uIdFound==0:
+            self.uniqueId=-1
+            print('No Matches Today')
+            return
+
+        #print(self.uniqueId)
+        sendData=self.getScoreCurrent(self.uniqueId)
+        print(sendData)
+
+    def getScoreCurrent(self,uniqueId):
+        data=""
+        uriParams={"apikey":self.apiKey,"unique_id":uniqueId}
+        resp=requests.get(self.getScore,params=uriParams)
+        data_json=resp.json()
+        #print(data_json)
+        try:
+            data="Here's the score: \n"+data_json["stat"]+'\n'+data_json['score']
+        except KeyError as e:
+            print(e)
+        return data
+
+
+if __name__ == '__main__':
+    score=Score()
+    score.getUniqueId()
